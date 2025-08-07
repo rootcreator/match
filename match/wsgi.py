37f6be256a32d7d596951c98bac723972bd7bb0c
Marcelinus Apakhade <maarrxxy@gmail.com>
2025-08-07 10:55:29 +0100

    Update wsgi.py

--- match/wsgi.py
+++ match/wsgi.py
@@ -1,19 +1,11 @@
-"""
-WSGI config for match project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 from whitenoise import WhiteNoise
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'match.settings')
 
 application = get_wsgi_application()
 
-application = WhiteNoise(application, root=os.path.join(BASE_DIR, 'staticfiles'))
+# Use the STATIC_ROOT path defined in settings.py
+application = WhiteNoise(application, root=settings.STATIC_ROOT)
